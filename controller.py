389518b80657f29af1e9a4b1e3b8548250e5e974
Hiroki Sugimura <sugimura@bitset.jp>
2019-07-22 19:37:10 +0900

    不要なものを削除

--- controller.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__, static_folder='build', static_url_path='')
-
-if __name__ == "__main__":
-    print(app.url_map)
-    app.run(host="0.0.0.0", port=10080)
